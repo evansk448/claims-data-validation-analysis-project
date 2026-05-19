6848b7183ad8d7e73a7f7ccb64b52f20fde8fb12
evansk448 <evanskyrie89@gmail.com>
2026-05-18 20:43:24 -0400

    Update analysis_queries.sql

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -6,26 +6,26 @@ FROM claims
 GROUP BY claimants.state
 ORDER BY total_claims DESC;
 
-> TOTAL PAYMENTS ISSUED PER CLAIM <
+## TOTAL PAYMENTS ISSUED PER CLAIM
 
 SELECT payments.claim_id, SUM(payments.payment_amount) AS total_paid
 FROM payments
 GROUP BY payments.claim_id;
 
-> total claims per policy type <
+## TOTAL CLAIMS PER POLICY TYPE
 
 SELECT policies.policy_type, COUNT(claims.claim_id) AS total_claims
 FROM claims
   JOIN policies ON policies.policy_id = claims.policy_id
 GROUP BY policies.policy_type;
 
-> average reserve amount per claim status <
+## AVERAGE RESERVE AMOUNT PER CLAIM STATUS
 
 SELECT claims.claim_status, AVG(claims.reserve_amount) AS average_reserve
 FROM claims
 GROUP BY claims.claim_status;
 
-> total payments grouped by payment status <
+## TOTAL PAYMENTS GROUPED BY PAYMENT STATUES
 
 SELECT payments.payment_status, SUM(payments.payment_amount) AS total_payments
 FROM payments
